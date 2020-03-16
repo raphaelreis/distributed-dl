e6a7160f3dd43299b43bf4a843b0f423e452eab3
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-03-16 11:01:42 +0100

    refoctor prototype

--- federated/federated_protocole_prototype_test.go
+++ federated/federated_protocole_prototype_test.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -57,17 +58,44 @@ func TestFederatedProtocole(t *testing.T) {
 
 	fmt.Printf("training: %d, val: %d, test: %d\n", len(train), len(test), len(test))
 
+	// Prepare networks and data
+	workCh := make(chan Example, numUsers)
+	nets := make([]*deep.Neural, numUsers)
+	for i := 0; i < len(numUsers); i++ {
+		nets[i] = deep.NewNeural(n.Config)
+	}
+	nonOverlappingData = train.SplitSize(numUsers)
+	var globalWeights [][][]float64
+
+
 	// Iterate over epochs
 	for e := 0; e < epochs; e++ {
-		var w_locals []float64
-		var loss_locals []float64
-		// Iterate over the number of users
-		for j := 0; j < numUsers; j++ {
-
+		
+		if e != 0 {
+			for i := 0; i < numUsers; i++ {
+				nets[i].ApplyWeights(globalWeights)
+			}
 		}
-	}
 
-	trainer.Train(globalNetwork, train, test, 500)
+		var wg sync.WaitGroup
+		// Iterate over the number of users
+		wg.Add(len(nonOverlappingData))
+		for i, b := range nonOverlappingData {
+			go funct(batch []Example) {
+				
+				// trainer = training.NewBatchTrainer(training.NewAdam(0.02, 0.9, 0.999, 1e-8), 1, 200, 8)
+				trainer.train(nets[i], batch, test, 1)
+
+
+				wg.Done(b)
+			}
+		}
+		wg.Wait()
+
+		globalWeights := nets.FederatedWeights('avg')
+
+
+	}
 }
 
 func load(path string) (training.Examples, error) {
