22567be0deeb5c1962cf8e6cbecce2d32c61ba71
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-03-19 16:11:04 +0100

    Implement Federated protocole.

--- federated/federated_protocole_prototype_test.go
+++ federated/federated_protocole_prototype_test.go
@@ -1,4 +1,4 @@
-package federated
+package federated_test
 
 import (
 	"bufio"
@@ -19,31 +19,31 @@ import (
 func TestFederatedProtocole(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 
-	train, err := load("/Users/Raphael/go/src/github.com/ldsec/dpnn/data/mnist_data/mnist_train.csv")
+	train, err := load("/Users/Raphael/go/src/github.com/ldsec/dpnn/data/mnist_data/normalized/mnist_train_norm.csv")
 	if err != nil {
 		panic(err)
 	}
-	test, err := load("/Users/Raphael/go/src/github.com/ldsec/dpnn/data/mnist_data/mnist_test.csv")
+	test, err := load("/Users/Raphael/go/src/github.com/ldsec/dpnn/data/mnist_data/normalized/mnist_test_norm.csv")
 	if err != nil {
 		panic(err)
 	}
 
-	for i := range train {
-		for j := range train[i].Input {
-			train[i].Input[j] = train[i].Input[j] / 255
-		}
-	}
-	for i := range test {
-		for j := range test[i].Input {
-			test[i].Input[j] = test[i].Input[j] / 255
-		}
-	}
+	// for i := range train {
+	// 	for j := range train[i].Input {
+	// 		train[i].Input[j] = train[i].Input[j] / 255
+	// 	}
+	// }
+	// for i := range test {
+	// 	for j := range test[i].Input {
+	// 		test[i].Input[j] = test[i].Input[j] / 255
+	// 	}
+	// }
 	test.Shuffle()
 	train.Shuffle()
 
 	// Constants
 	numUsers := 5
-	epochs := 10
+	epochs := 1
 
 	globalNetwork := deep.NewNeural(&deep.Config{
 		Inputs:     len(train[0].Input),
@@ -59,18 +59,17 @@ func TestFederatedProtocole(t *testing.T) {
 	fmt.Printf("training: %d, val: %d, test: %d\n", len(train), len(test), len(test))
 
 	// Prepare networks and data
-	workCh := make(chan Example, numUsers)
+	// workCh := make(chan Example, numUsers)
 	nets := make([]*deep.Neural, numUsers)
-	for i := 0; i < len(numUsers); i++ {
-		nets[i] = deep.NewNeural(n.Config)
+	for i := 0; i < numUsers; i++ {
+		nets[i] = deep.NewNeural(globalNetwork.Config)
 	}
-	nonOverlappingData = train.SplitSize(numUsers)
+	nonOverlappingData := train.SplitSize(numUsers)
 	var globalWeights [][][]float64
-
 
 	// Iterate over epochs
 	for e := 0; e < epochs; e++ {
-		
+
 		if e != 0 {
 			for i := 0; i < numUsers; i++ {
 				nets[i].ApplyWeights(globalWeights)
@@ -81,21 +80,69 @@ func TestFederatedProtocole(t *testing.T) {
 		// Iterate over the number of users
 		wg.Add(len(nonOverlappingData))
 		for i, b := range nonOverlappingData {
-			go funct(batch []Example) {
-				
+			go func(batch []training.Example) {
+
 				// trainer = training.NewBatchTrainer(training.NewAdam(0.02, 0.9, 0.999, 1e-8), 1, 200, 8)
-				trainer.train(nets[i], batch, test, 1)
+				trainer.Train(nets[i], batch, test, 1)
 
-
-				wg.Done(b)
-			}
+				wg.Done()
+			}(b)
 		}
 		wg.Wait()
 
-		globalWeights := nets.FederatedWeights('avg')
-
-
+		globalWeights = federatedWeights(nets, numUsers)
 	}
+}
+
+func federatedWeights(nets []*deep.Neural, numUsers int) [][][]float64 {
+
+	fedWeights := nets[0].Weights()
+	for i, net := range nets {
+		if i != 0 {
+			fedWeights = add3d(fedWeights, net.Weights())
+		}
+	}
+	nu := float64(numUsers)
+	for i := range fedWeights {
+		for j := range fedWeights[i] {
+			for k := range fedWeights[i][j] {
+				fedWeights[i][j][k] /= nu
+			}
+		}
+	}
+
+	return fedWeights
+}
+
+func add(a, b []float64) []float64 {
+
+	sum := make([]float64, len(a))
+	for i, a_ := range a {
+		sum[i] = a_ + b[i]
+	}
+	return sum
+}
+
+func add2d(a, b [][]float64) [][]float64 {
+
+	sum := make([][]float64, len(a))
+	for i, a_ := range a {
+		sum[i] = make([]float64, len(a_))
+	}
+
+	for i, a_ := range a {
+		sum[i] = add(a_, b[i])
+	}
+
+	return sum
+}
+
+func add3d(a, b [][][]float64) [][][]float64 {
+	sum := a
+	for i := range a {
+		sum[i] = add2d(sum[i], b[i])
+	}
+	return sum
 }
 
 func load(path string) (training.Examples, error) {
