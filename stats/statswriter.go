3290f79ed409489935263d85b1346c1318f47e07
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-03-26 20:59:21 +0100

    start statwriter implementation

---
+++ stats/statswriter.go
@@ -0,0 +1,22 @@
+package stats
+
+import (
+	"os"
+
+	"github.com/patrikeh/go-deep"
+)
+
+// StatWriter write formatted neural net metrics to csv file
+type StatWriter struct {
+	folder string
+}
+
+// NewStatsWriter creates a StatsWriter
+func NewStatsWriter(folder string) *StatWriter {
+	return &StatWriter{folder}
+}
+
+// Init initialize the stat writer
+func (w *StatWriter) Init(master *deep.Neural, workers []*deep.Neural) {
+	os.Mkdir(w.folder)
+}
